aa6f12c9713a1c5e22c253c6d7b393123853ed4f
仁泉 大輔 <daisuke_niizumi@hitoshisen-daisuke-no-MacBook-Pro.local>
2015-08-26 18:22:36 +0900

    Initial Commit

---
+++ Camerar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Camerar
+//
+//  Created by 仁泉 大輔 on 8/26/15.
+//  Copyright (c) 2015 仁泉 大輔. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Camerar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Camerar
+//
+//  Created by 仁泉 大輔 on 8/26/15.
+//  Copyright (c) 2015 仁泉 大輔. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
